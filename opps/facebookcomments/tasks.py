ccba1a77b6fd3acd778648922ea9224b29011c87
Ellison Leao <ellisonleao@gmail.com>
2013-05-16 15:46:06 -0300

    Add batch requests by default using facebook APP_ID and API_SECRET

--- opps/facebookcomments/tasks.py
+++ opps/facebookcomments/tasks.py
@@ -3,6 +3,7 @@ from urllib import urlencode
 
 from django.conf import settings
 from django.utils import timezone
+from django.core.exceptions import ImproperlyConfigured
 
 from .models import TopComment
 
@@ -10,18 +11,32 @@ from opps.articles.models import Post
 from celery.decorators import periodic_task
 from celery.task.schedules import crontab
 from facepy import GraphAPI
+from facepy.utils import get_application_access_token
 
 @periodic_task(run_every=crontab(hour="23", minute="59", day_of_week="*"))
 def update_top_comments():
     """
     updates the top comments
     """
+    if not hasattr(settings, 'FACEBOOK_APP_ID') :
+        raise ImproperlyConfigured('You must have an FACEBOOK_APP_ID and a '
+                                   ' FACEBOOK_API_SECRET in your settings.py')
+
+    today = timezone.now()
+    days_ago = today - timezone.timedelta(days=7)
+
     posts = Post.objects.all_published().filter(site=settings.SITE_ID)
-    posts = posts[:10]
-    #requests = []
-    #count = 0
+    posts = posts.filter(date_available__range=(days_ago, today))
+
+    requests = []
+    count = 0
+
+    token =  get_application_access_token(settings.FACEBOOK_APP_ID,
+                                          settings.FACEBOOK_API_SECRET)
+
+    f = GraphAPI(token)
+
     for post in posts.iterator():
-        """
         # FOR BATCH REQUEST WE NEED A VALID TOKEN OR APP_ID
         # MAX 50 BATCH REQUESTS PER TIME
 
@@ -47,24 +62,15 @@ def update_top_comments():
                     try:
                         top = TopComment.objects.get(post=post)
                         top.comment_count = comment_count
+                        top.save()
                     except TopComment.DoesNotExist:
                         TopComment.objects.create(post=post,
                                                   comment_count=comment_count,
                                                   user=post.user,
                                                   site=post.site)
             count = 0
+            requests = []
 
-        """
-        comment_count = get_comment_count(post)
-        if comment_count > 0:
-            try:
-                top = TopComment.objects.get(post=post)
-                top.comment_count = comment_count
-            except TopComment.DoesNotExist:
-                TopComment.objects.create(post=post,
-                                          comment_count=comment_count,
-                                          user=post.user,
-                                          site=post.site)
 
 def get_comment_count(obj):
     """
